44d52fb26c5214e53a62453e8708b3d5b0ff6560
fabio <f.chibana@liberra.com>
2022-11-17 14:16:56 +0900

    bla

--- main.py
+++ main.py
@@ -63,205 +63,243 @@ def diff_drive(states, controls):
     return f
 
 
-def mecanum_whell(states, controls):
-    """ Mecanum wheel transfer function which can be found here:
-    https://www.researchgate.net/publication/334319114_Model_Predictive_Control_for_a_Mecanum-wheeled_robot_in_Dynamical_Environments
+# def mecanum_whell(states, controls):
+#     """ Mecanum wheel transfer function which can be found here:
+#     https://www.researchgate.net/publication/334319114_Model_Predictive_Control_for_a_Mecanum-wheeled_robot_in_Dynamical_Environments
 
 
-    Returns:
-        [type]: [description]
-    """
+#     Returns:
+#         [type]: [description]
+#     """
 
-    # Robot specs
-    # rob_diam = 0.3  # diameter of the robot
-    wheel_radius = 1  # wheel radius
-    Lx = 0.3  # L in J Matrix (half robot x-axis length)
-    Ly = 0.3  # l in J Matrix (half robot y-axis length)
+#     # Robot specs
+#     # rob_diam = 0.3  # diameter of the robot
+#     wheel_radius = 1  # wheel radius
+#     Lx = 0.3  # L in J Matrix (half robot x-axis length)
+#     Ly = 0.3  # l in J Matrix (half robot y-axis length)
 
-    # discretization model (e.g. x2 = f(x1, v, t) = x1 + v * dt)
-    rot_3d_z = ca.vertcat(ca.horzcat(cos(theta), -sin(theta), 0),
-                          ca.horzcat(sin(theta), cos(theta), 0),
-                          ca.horzcat(0, 0, 1)
-                          )
-    J = (wheel_radius / 4) * ca.DM([[1, 1, 1, 1],
-                                    [-1, 1, 1, -1],
-                                    [-1 / (Lx + Ly), 1 / (Lx + Ly), -1 / (Lx + Ly), 1 / (Lx + Ly)]])
-    # RHS = states + J @ controls * step_horizon  # Euler discretization
-    RHS = rot_3d_z @ J @ controls
-    # maps controls from [va, vb, vc, vd].T to [vx, vy, omega].T
-    f = ca.Function('f', [states, controls], [RHS])
+#     # discretization model (e.g. x2 = f(x1, v, t) = x1 + v * dt)
+#     rot_3d_z = ca.vertcat(ca.horzcat(cos(theta), -sin(theta), 0),
+#                           ca.horzcat(sin(theta), cos(theta), 0),
+#                           ca.horzcat(0, 0, 1)
+#                           )
+#    J = (wheel_radius / 4) * ca.DM(
+#        [[1, 1, 1, 1],
+#        [-1, 1, 1, -1],
+#        [-1 / (Lx + Ly), 1 / (Lx + Ly), -1 / (Lx + Ly), 1 / (Lx + Ly)]])
+#     # RHS = states + J @ controls * step_horizon  # Euler discretization
+#     RHS = rot_3d_z @ J @ controls
+#     # maps controls from [va, vb, vc, vd].T to [vx, vy, omega].T
+#     f = ca.Function('f', [states, controls], [RHS])
 
-    return f
-
-#
-# Input vars
-#
+#     return f
 
 
-# Loss function weights
-# State loss function
-# Q_x = 1
-# Q_y = 5
-# Q_theta = 0.1
-Q_x = 1
-Q_y = 10
-Q_theta = 0.01
+def cost_function(n_states, N, X, U, P):
 
-# Control loss function
-# R_v = 0.5
-# R_omega = 0.05
-R_v = 0.5
-R_omega = 0.05
+    Q_x = 1
+    Q_y = 1
+    Q_theta = 0.01
+    R_v = 0.5
+    R_omega = 0.05
 
-# Horizon specs
-step_horizon = 0.2  # time between steps in seconds
-N = 10              # number of look ahead steps
+    Q = ca.diagcat(Q_x, Q_y, Q_theta)
+    R = ca.diagcat(R_v, R_omega)
 
-# Simulation specs
-sim_time = 200      # simulation time
+    cost_fn = 0
+    for k in range(N):
+        st = X[:, k]
+        con = U[:, k]
 
+        state_loss = (st - P[n_states:]).T @ Q @ (st - P[n_states:])
+        control_loss = con.T @ R @ con
+        cost_fn += state_loss + control_loss
 
-# Initial state
-x_init = 0
-y_init = 0
-theta_init = 0
-# Target state
-x_target = 10
-y_target = 10
-theta_target = - pi / 2
-theta_target = theta_target % (2*pi)
-
-# Control bounds
-# TODO: use turtlebot specs
-v_max = 0.6
-v_min = - v_max
-omega_max = pi / 4
-omega_min = - omega_max
+    return cost_fn
 
 
-#
-# Symbolic
-#
+def constraint_equations(n_states, N, step_horizon, X, U, P, f):
+    g = X[:, 0] - P[:n_states]  # constraints in the equation
 
-# state symbolic variables
-x = ca.SX.sym('x')
-y = ca.SX.sym('y')
-theta = ca.SX.sym('theta')
-states = ca.vertcat(x, y, theta)
-n_states = states.numel()
+    # runge kutta
+    for k in range(N):
 
-# control symbolic variables
-v = ca.SX.sym('v')
-omega = ca.SX.sym('omega')
-controls = ca.vertcat(v, omega)
-n_controls = controls.numel()
+        st = X[:, k]
+        con = U[:, k]
 
-# matrix containing all states over all time steps +1 (each column is a state vector)
-X = ca.SX.sym('X', n_states, N + 1)
-# matrix containing all control actions over all time steps (each column is an action vector)
-U = ca.SX.sym('U', n_controls, N)
+        # state_loss = (st - P[n_states:]).T @ Q @ (st - P[n_states:])
+        # control_loss = con.T @ R @ con
+        # cost_fn += state_loss + control_loss
 
-# column vector for storing initial state and target state
-P = ca.SX.sym('P', n_states + n_states)
+        st_next = X[:, k+1]
+        st_next_RK4 = rk4(f, st, con, step_horizon)
+        g = ca.vertcat(g, st_next - st_next_RK4)
 
-# state weights matrix (Q_X, Q_Y, Q_THETA)
-Q = ca.diagcat(Q_x, Q_y, Q_theta)
-# controls weights matrix
-R = ca.diagcat(R_v, R_omega)
-
-f = diff_drive(states, controls)
-
-cost_fn = 0  # cost function
-g = X[:, 0] - P[:n_states]  # constraints in the equation
+    return g
 
 
-# runge kutta
-for k in range(N):
+def main():
 
-    st = X[:, k]
-    con = U[:, k]
+    #
+    # Input vars
+    #
 
-    state_loss = (st - P[n_states:]).T @ Q @ (st - P[n_states:])
-    control_loss = con.T @ R @ con
-    cost_fn += state_loss + control_loss
+    # Loss function weights
+    # State loss function
+    # Q_x = 1
+    # Q_y = 5
+    # Q_theta = 0.1
+    # Q_x = 1
+    # Q_y = 10
+    # Q_theta = 0.01
 
-    st_next = X[:, k+1]
-    st_next_RK4 = rk4(f, st, con, step_horizon)
-    g = ca.vertcat(g, st_next - st_next_RK4)
+    # Control loss function
+    # R_v = 0.5
+    # R_omega = 0.05
+    # R_v = 0.5
+    # R_omega = 0.05
 
+    # Horizon specs
+    step_horizon = 0.2  # time between steps in seconds
+    N = 10              # number of look ahead steps
 
-# Vector with optimization variables
-# Example: 3x11 ---> 33x1 where 3=states, 11=N+1
-opt_vars = ca.vertcat(X.reshape((-1, 1)), U.reshape((-1, 1)))
-nlp_prob = {'f': cost_fn,
-            'x': opt_vars,
-            'g': g,
-            'p': P}
+    # Simulation specs
+    sim_time = 200      # simulation time
 
-opts = {'ipopt': {'max_iter': 2000,
-                  'print_level': 0,
-                  'acceptable_tol': 1e-8,
-                  'acceptable_obj_change_tol': 1e-6},
-        'print_time': 0}
+    # Initial state
+    x_init = 0
+    y_init = 0
+    theta_init = 0
+    # Target state
+    x_target = 10
+    y_target = 10
+    theta_target = - pi / 2
+    theta_target = theta_target % (2*pi)
 
-solver = ca.nlpsol('solver', 'ipopt', nlp_prob, opts)
+    # Control bounds
+    # TODO: use turtlebot specs
+    v_max = 0.6
+    v_min = - v_max
+    omega_max = pi / 4
+    omega_min = - omega_max
 
+    #
+    # Symbolic
+    #
 
-# Define lower and upper bounds for optimization variables
-# and constraint functions
+    # state symbolic variables
+    x = ca.SX.sym('x')
+    y = ca.SX.sym('y')
+    theta = ca.SX.sym('theta')
+    states = ca.vertcat(x, y, theta)
+    n_states = states.numel()
 
-lbx = ca.DM.zeros((n_states*(N+1) + n_controls*N, 1))
-ubx = ca.DM.zeros((n_states*(N+1) + n_controls*N, 1))
+    # control symbolic variables
+    v = ca.SX.sym('v')
+    omega = ca.SX.sym('omega')
+    controls = ca.vertcat(v, omega)
+    n_controls = controls.numel()
 
-lbx[0: n_states*(N+1): n_states] = -ca.inf     # x lower bound
-lbx[1: n_states*(N+1): n_states] = -ca.inf     # y lower bound
-lbx[2: n_states*(N+1): n_states] = -ca.inf     # theta lower bound
+    # matrix containing all states over all time steps +1 (each column is a state vector)
+    X = ca.SX.sym('X', n_states, N + 1)
+    # matrix containing all control actions over all time steps (each column is an action vector)
+    U = ca.SX.sym('U', n_controls, N)
 
-ubx[0: n_states*(N+1): n_states] = ca.inf      # y upper bound
-ubx[1: n_states*(N+1): n_states] = ca.inf      # y upper bound
-ubx[2: n_states*(N+1): n_states] = ca.inf      # theta upper bound
+    # column vector for storing initial state and target state
+    P = ca.SX.sym('P', n_states + n_states)
 
-lbx[n_states * (N + 1)::n_controls] = v_min
-lbx[n_states * (N + 1) + 1::n_controls] = omega_min
-ubx[n_states * (N + 1)::n_controls] = v_max
-ubx[n_states * (N + 1) + 1::n_controls] = omega_max
+    # state weights matrix (Q_X, Q_Y, Q_THETA)
+    # Q = ca.diagcat(Q_x, Q_y, Q_theta)
+    # controls weights matrix
+    # R = ca.diagcat(R_v, R_omega)
 
-args = {'lbg': ca.DM.zeros((n_states*(N+1), 1)),  # constraints lower bound
-        'ubg': ca.DM.zeros((n_states*(N+1), 1)),  # constraints upper bound
-        'lbx': lbx,
-        'ubx': ubx}
+    f = diff_drive(states, controls)
 
+    # cost_fn = 0  # cost function
+    # g = X[:, 0] - P[:n_states]  # constraints in the equation
 
-# Set up main loop
+    # runge kutta
+    # for k in range(N):
 
-t0 = 0
-state_init = ca.DM([x_init, y_init, theta_init])        # initial state
-state_target = ca.DM([x_target, y_target, theta_target])  # target state
+    #     st = X[:, k]
+    #     con = U[:, k]
 
-t = ca.DM(t0)
+    #     state_loss = (st - P[n_states:]).T @ Q @ (st - P[n_states:])
+    #     control_loss = con.T @ R @ con
+    #     cost_fn += state_loss + control_loss
 
-u0 = ca.DM.zeros((n_controls, N))          # initial control
-X0 = ca.repmat(state_init, 1, N+1)         # initial state full
+    #     st_next = X[:, k+1]
+    #     st_next_RK4 = rk4(f, st, con, step_horizon)
+    #     g = ca.vertcat(g, st_next - st_next_RK4)
 
+    # Vector with optimization variables
+    # Example: 3x11 ---> 33x1 where 3=states, 11=N+1
+    opt_vars = ca.vertcat(X.reshape((-1, 1)), U.reshape((-1, 1)))
+    nlp_prob = {
+        # 'f': cost_fn,
+        "f": cost_function(n_states, N, X, U, P),
+        'x': opt_vars,
+        # 'g': g,
+        "g": constraint_equations(n_states, N, step_horizon, X, U, P, f),
+        'p': P
+    }
 
-mpc_iter = 0
-cat_states = DM2Arr(X0)
-cat_controls = DM2Arr(u0[:, 0])
-times = np.array([[0]])
+    opts = {'ipopt': {'max_iter': 2000,
+                      'print_level': 0,
+                      'acceptable_tol': 1e-8,
+                      'acceptable_obj_change_tol': 1e-6},
+            'print_time': 0}
 
+    solver = ca.nlpsol('solver', 'ipopt', nlp_prob, opts)
 
-###############################################################################
+    # Define lower and upper bounds for optimization variables
+    # and constraint functions
 
-if __name__ == '__main__':
+    lbx = ca.DM.zeros((n_states*(N+1) + n_controls*N, 1))
+    ubx = ca.DM.zeros((n_states*(N+1) + n_controls*N, 1))
+
+    lbx[0: n_states*(N+1): n_states] = -ca.inf     # x lower bound
+    lbx[1: n_states*(N+1): n_states] = -ca.inf     # y lower bound
+    lbx[2: n_states*(N+1): n_states] = -ca.inf     # theta lower bound
+
+    ubx[0: n_states*(N+1): n_states] = ca.inf      # y upper bound
+    ubx[1: n_states*(N+1): n_states] = ca.inf      # y upper bound
+    ubx[2: n_states*(N+1): n_states] = ca.inf      # theta upper bound
+
+    lbx[n_states * (N + 1)::n_controls] = v_min
+    lbx[n_states * (N + 1) + 1::n_controls] = omega_min
+    ubx[n_states * (N + 1)::n_controls] = v_max
+    ubx[n_states * (N + 1) + 1::n_controls] = omega_max
+
+    args = {'lbg': ca.DM.zeros((n_states*(N+1), 1)),  # constraints lower bound
+            'ubg': ca.DM.zeros((n_states*(N+1), 1)),  # constraints upper bound
+            'lbx': lbx,
+            'ubx': ubx}
+
+    # Set up main loop
+
+    t0 = 0
+    current_state = ca.DM([x_init, y_init, theta_init])        # initial state
+    target_state = ca.DM([x_target, y_target, theta_target])  # target state
+
+    # t = ca.DM(t0)
+
+    u0 = ca.DM.zeros((n_controls, N))          # initial control
+    X0 = ca.repmat(current_state, 1, N+1)         # initial state full
+
+    mpc_iter = 0
+    cat_states = DM2Arr(X0)
+    cat_controls = DM2Arr(u0[:, 0])
+    times = np.array([[0]])
 
     main_loop = time()  # return time in sec
 
-    while (ca.norm_2(state_init - state_target) > 1e-1) and (mpc_iter * step_horizon < sim_time):
+    while (ca.norm_2(current_state - target_state) > 1e-1) and (mpc_iter * step_horizon < sim_time):
 
         t1 = time()
 
         # current and target states
-        args['p'] = ca.vertcat(state_init, state_target)
+        args['p'] = ca.vertcat(current_state, target_state)
         # optimization variable current state
         args['x0'] = ca.vertcat(ca.reshape(X0, n_states*(N+1), 1), ca.reshape(u0, n_controls*N, 1))
 
@@ -271,28 +309,29 @@ if __name__ == '__main__':
                      lbg=args['lbg'],
                      ubg=args['ubg'],
                      p=args['p'])
-
+        
         u = ca.reshape(sol['x'][n_states * (N + 1):], n_controls, N)
         X0 = ca.reshape(sol['x'][: n_states * (N+1)], n_states, N+1)
 
         cat_states = np.dstack((cat_states, DM2Arr(X0)))
         cat_controls = np.vstack((cat_controls, DM2Arr(u[:, 0])))
-        t = np.vstack((t, t0))
+        # t = np.vstack((t, t0))
+        times = np.vstack((times, time()-t1))
 
-        t0, state_init, u0 = shift_time_step_rk4(step_horizon, t0, state_init, u, f)
+        t0, current_state, u0 = shift_time_step_rk4(step_horizon, t0, current_state, u, f)
 
         X0 = ca.horzcat(X0[:, 1:], ca.reshape(X0[:, -1], -1, 1))
 
         # xx ...
-        t2 = time()
+        # t2 = time()
         # print(mpc_iter)
         # print(t2-t1)
-        times = np.vstack((times, t2-t1))
+        # times = np.vstack((times, t2-t1))
 
         mpc_iter = mpc_iter + 1
 
     main_loop_time = time()
-    ss_error = ca.norm_2(state_init - state_target)
+    ss_error = ca.norm_2(current_state - target_state)
 
     print('\n\n')
     print('Total time: ', main_loop_time - main_loop)
@@ -302,3 +341,10 @@ if __name__ == '__main__':
     # simulate
     simulate(cat_states, cat_controls, times, step_horizon, N,
              np.array([x_init, y_init, theta_init, x_target, y_target, theta_target]), save=False)
+
+###############################################################################
+
+
+if __name__ == '__main__':
+
+    main()
